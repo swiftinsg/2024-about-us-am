522f239d0ca83a6e69cad0d35d69aa9050693345
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2024-07-06 05:47:12 +0800

    Initial Commit

---
+++ about-us-am/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  about-us-am
+//
+//  Created by Jia Chen Yee on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ about-us-am/about_us_amApp.swift
@@ -0,0 +1,17 @@
+//
+//  about_us_amApp.swift
+//  about-us-am
+//
+//  Created by Jia Chen Yee on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct about_us_amApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
