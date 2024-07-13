f7976a224cdbb31a47f948873c900d11ffcd9e11
Calvin Abad <calvin.abad@gmail.com>
2024-07-13 11:50:57 +0800

    Name card for myself in the app

---
+++ about-us-am/gp5 views/BryanView.swift
@@ -0,0 +1,24 @@
+//
+//  BryanView.swift
+//  about-us-am
+//
+//  Created by Bryan Nguyen on 13/7/24.
+//
+
+import SwiftUI
+
+struct BryanView: View {
+    var body: some View {
+        ZStack{
+            Image("goodmorning")
+            VStack{
+                Text("Hello, future apple fruit innovaties")
+                Image("hitlerandstalin")
+            }
+        }
+    }
+}
+
+#Preview {
+    BryanView()
+}
