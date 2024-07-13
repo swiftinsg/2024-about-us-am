5e2fea84ddd5745c63e9d53460a0f30ecdc176e1
YJ Soon <yjsoon@tinkertanker.com>
2024-07-13 10:22:30 +0800

    Updated with group views and gitignore

--- about-us-am/Group2View.swift
+++ about-us-am/Group2View.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct Group2View: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            /*@START_MENU_TOKEN@*/Text("This is person 1! Replace me with a view.")/*@END_MENU_TOKEN@*/
+                .tabItem {
+                    Label("Person 1", systemImage: "person.circle.fill")
+                }
+            
+            /*@START_MENU_TOKEN@*/Text("This is person 2! Replace me with a view.")/*@END_MENU_TOKEN@*/
+                .tabItem {
+                    Label("Person 2", systemImage: "person")
+                }
+            
+            /*@START_MENU_TOKEN@*/Text("This is person 3! Replace me with a view.")/*@END_MENU_TOKEN@*/
+                .tabItem {
+                    Label("Person 3", systemImage: "person.fill.turn.down")
+                }
+        }
     }
 }
 
