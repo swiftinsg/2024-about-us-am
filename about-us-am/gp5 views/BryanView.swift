be5bdc03c13a10b8b89e5369d446ad2f8ef6467f
Bryan Nguyen <bryannguyenngo@gmail.com>
2024-07-13 11:50:35 +0800

    i fixed things

--- about-us-am/gp5 views/BryanView.swift
+++ about-us-am/gp5 views/BryanView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct BryanView: View {
+    let backgroundGradient = LinearGradient(
+        colors: [Color.red, Color.blue, Color.gray],
+        startPoint: .top, endPoint: .bottom)
     var body: some View {
         ZStack{
+            backgroundGradient
+                .ignoresSafeArea()
             Image("goodmorning")
             VStack{
                 Text("Hello, future apple fruit innovaties")
