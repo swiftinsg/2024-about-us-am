69e53106c0bf0035b71bd8890159baf1f941ff03
Yuk Jun Lim <limyukjun@gmail.com>
2024-07-13 11:51:56 +0800

    My view

---
+++ about-us-am/YukJun.swift
@@ -0,0 +1,18 @@
+//
+//  YukJun.swift
+//  about-us-am
+//
+//  Created by Yuk Jun Lim on 13/7/24.
+//
+
+import SwiftUI
+
+struct YukJun: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YukJun()
+}
