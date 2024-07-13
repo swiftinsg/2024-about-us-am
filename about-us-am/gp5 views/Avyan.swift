55a94874edbd3f642abd8ab7c7b01c7fd1bb75ed
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-07-13 11:28:06 +0800

    Added Avyan's Page

---
+++ about-us-am/gp5 views/Avyan.swift
@@ -0,0 +1,18 @@
+//
+//  Avyan.swift
+//  about-us-am
+//
+//  Created by Avyan Mehra on 13/7/24.
+//
+
+import SwiftUI
+
+struct Avyan: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Avyan()
+}
