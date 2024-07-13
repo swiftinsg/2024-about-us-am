19e25409bd7646f1722af9845d0547ae77172f2d
Sophia <papayalimernh321@gmail.com>
2024-07-13 11:57:13 +0800

    Added my section

---
+++ about-us-am/Group1Member3View.swift
@@ -0,0 +1,18 @@
+//
+//  Group1Member3View.swift
+//  about-us-am
+//
+//  Created by T Krobot on 13/7/24.
+//
+
+import SwiftUI
+
+struct Group1Member3View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Group1Member3View()
+}
