8ffc89be51d75c58bb38edf2763bc1c979718752
Calvin Abad <calvin.abad@gmail.com>
2024-07-13 11:35:03 +0800

    Made a new file for my personal name card

---
+++ about-us-am/Group 5 views/Group5MemberCalvinView.swift
@@ -0,0 +1,18 @@
+//
+//  Group5MemberCalvinView.swift
+//  about-us-am
+//
+//  Created by Calvin Abad on 13/7/24.
+//
+
+import SwiftUI
+
+struct Group5MemberCalvinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Group5MemberCalvinView()
+}
